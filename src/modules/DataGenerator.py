5cd96e2d89616f5bd2dce7dffa1172d36dc9c941
Olegas13 <oleg13000@gmail.com>
2017-02-16 21:14:12 +0300

    update  datagenerator

--- src/modules/DataGenerator.py
+++ src/modules/DataGenerator.py
@@ -27,48 +27,15 @@ class DataGenerator:
         #Заменяем пробелы и преобразуем строку к нижнему регистру
         name = name.lower()
         #
-        transtable = (
-            ## Маленькие буквы
-            # three-symbols
-            (u"щ", u"sch"),
-            # two-symbols
-            (u"ё", u"yo"),
-            (u"ж", u"zh"),
-            (u"ц", u"ts"),
-            (u"ч", u"ch"),
-            (u"ш", u"sh"),
-            (u"ы", u"yi"),
-            (u"ю", u"yu"),
-            (u"я", u"ya"),
-            # one-symbol
-            (u"а", u"a"),
-            (u"б", u"b"),
-            (u"в", u"v"),
-            (u"г", u"g"),
-            (u"д", u"d"),
-            (u"е", u"e"),
-            (u"з", u"z"),
-            (u"и", u"i"),
-            (u"й", u"j"),
-            (u"к", u"k"),
-            (u"л", u"l"),
-            (u"м", u"m"),
-            (u"н", u"n"),
-            (u"о", u"o"),
-            (u"п", u"p"),
-            (u"р", u"r"),
-            (u"с", u"s"),
-            (u"т", u"t"),
-            (u"у", u"u"),
-            (u"ф", u"f"),
-            (u"х", u"h"),
-            (u"э", u"e"),
-            (u"ь", u""),
+        transtable = ((u"щ", u"sch"), (u"ё", u"yo"), (u"ж", u"zh"), (u"ц", u"ts"), (u"ч", u"ch"),
+            (u"ш", u"sh"), (u"ы", u"yi"), (u"ю", u"yu"), (u"я", u"ya"), (u"а", u"a"), (u"б", u"b"),
+            (u"в", u"v"), (u"г", u"g"), (u"д", u"d"), (u"е", u"e"), (u"з", u"z"), (u"и", u"i"),
+            (u"й", u"j"), (u"к", u"k"),  (u"л", u"l"), (u"м", u"m"),  (u"н", u"n"), (u"о", u"o"),
+            (u"п", u"p"), (u"р", u"r"), (u"с", u"s"), (u"т", u"t"), (u"у", u"u"), (u"ф", u"f"),
+            (u"х", u"h"), (u"э", u"e"), (u"ь", u""),
         )
-        #перебираем символы в таблице и заменяем
         for symb_in, symb_out in transtable:
             name = name.replace(symb_in, symb_out)
-        #возвращаем переменную
         return name
     
 
@@ -83,8 +50,10 @@ class DataGenerator:
 
             contact["name"] = self.__getRandomName(random.randint(1,3))
             contact["phoneNumber"] = "+7" + str(random.randint(900, 999)) + str(random.randint(1000000, 9999999))
-            contact["email"] = "".join(self.getTranslateWord(contact["name"]).split(" ")) + "@gmail.com"
-            contact["site"] = "".join([self.getTranslateWord(i) for i in contact["name"].split(" ")]) + random.choice((".ru", ".com", ".uk", ".us"))
+            contact["email"] = "".join(self.getTranslateWord(contact["name"]).split(" ")) + \
+                                random.choice(("@gmail.com", "@mail.ru", "@yandex.ru", "@rambler.ru"))
+            contact["site"] = "".join([self.getTranslateWord(i) for i in contact["name"].split(" ")]) + \
+                                random.choice((".ru", ".com", ".uk", ".us"))
 
             reliability["numBrokeSupply"] = random.randint(0, 100)
             reliability["numLawsuitsNow"] = random.randint(0, 5)
